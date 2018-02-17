3b777dab3603ba217d1eda33e7757fa851675152
byrrice <32532726+byrrice@users.noreply.github.com>
2018-02-17 16:06:26 -0500

    Added keypress mapping

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,10 +1,21 @@
 console.log("hello");
 
+//ctrl + space
+var map = {17: false, 192 :false};
 
+$(document).keydown(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = true;
+        if (map[17] && map[192]) {
+            var srcList = $('img').map(function() {
+                return this.src;
+            }).get();
+            console.log(srcList);
+        }
+    }
+}).keyup(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = false;
+    }
+});
 
-var srcList = $('img').map(function() {
-    return this.src;
-}).get();
-
-
-console.log(srcList);
