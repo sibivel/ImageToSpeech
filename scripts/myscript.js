51cb3dc9f9ab170628f2580b75dcc8bf835dfbb6
byrrice <32532726+byrrice@users.noreply.github.com>
2018-02-17 16:44:45 -0500

    Added basic text to speech framework

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,24 +1,47 @@
 console.log("hello");
 
-//ctrl + space
-var map = {17: false, 192 :false};
+//ctrl + tilda
+var map = {17: false, 192: false, 27: false, 16: false};
+var readimage = false
+var index = 0
+var length = 0
 
 $(document).keydown(function(e) {
     if (e.keyCode in map) {
         map[e.keyCode] = true;
         if (map[17] && map[192]) {
+            readimage = true;
             var srcList = $('img').map(function() {
                 return this.src;
             }).get();
             console.log(srcList);
+            index = 0;
+            length = srcList.length;
             if( srcList.length == 1){
-                speak("We have " + srcList.length + " image. Press escape to quit, or press the tilda key to play the next image discussion. ");
+                speak("We have " + srcList.length + " image. Press escape to quit, or press the left shift key to play the next image discussion. ");
             }
             else{
-                speak("We have " + srcList.length + " images. Press escape to quit, or press the tilda key to play the next image discussion. ");
+                speak("We have " + srcList.length + " images. Press escape to quit, or press the left shift key to play the next image discussion. ");
             }
-            
         }
+        if (map[27]){
+            readimage = false;
+            speak("Program Exited.");
+        }
+        else if (map[16] && readimage){
+            ++index;
+            if (index < length){
+                speak("This is the description of image number " + index);
+                //Google Vision code
+            }
+            else {
+                speak("There are no more images left in this list");
+                index = 0;
+                length = 0;
+                readimage = false;
+            }
+        }
+
     }
 }).keyup(function(e) {
     if (e.keyCode in map) {
